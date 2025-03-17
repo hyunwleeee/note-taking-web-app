4217a02f32fc4b6c409b3ecfac43f0b768099c48
hyunwleeee <hwlee3928@gmail.com>
2025-03-17 21:17:22 +0900

    remove: assets images to ./src

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import GlobalStyle from '@styles/globalStyle';
 import Theme from '@styles/theme';
 import { ThemeProvider } from 'styled-components';
+import Logo from './components/ui/header/Logo';
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
+      <Logo />
       <h1>hyunwlee</h1>
     </ThemeProvider>
   );
