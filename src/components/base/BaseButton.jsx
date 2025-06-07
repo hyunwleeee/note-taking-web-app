fd419a6e9f51cb0be060b22159565fdad074a0db
hyunwleeee <hwlee3928@gmail.com>
2025-06-07 23:15:13 +0900

    feat: theme ghost 추가

--- src/components/base/BaseButton.jsx
+++ src/components/base/BaseButton.jsx
@@ -11,12 +11,13 @@ function BaseButton({
 }) {
   return (
     <StyledButton
-      className={clsx(theme, restProps.className)}
       {...restProps}
+      className={clsx(theme, restProps.className)}
     >
       {leftIcon && <span className='left'>{leftIcon}</span>}
       {texture && <span className='text'>{texture}</span>}
       {rightIcon && <span className='right'>{rightIcon}</span>}
+      {restProps.children}
     </StyledButton>
   );
 }
@@ -24,7 +25,7 @@ function BaseButton({
 const disabledStyles = css`
   &.primary:disabled,
   &.secondary:disabled,
-  &.border:disabled {
+  &.border:disabled { 
     color: ${({ theme }) => theme.colors.neutral300};
     background: ${({ theme }) => theme.colors.neutral100};
     cursor: not-allowed;
@@ -45,11 +46,15 @@ const disabledStyles = css`
 const StyledButton = styled.button`
   all: unset;
 
-  ${({ theme }) => theme.typography.textPreset4};
+  ${({ theme }) => theme.typography.textPreset6};
   padding: ${({ theme }) => `${theme.spacing[150]} ${theme.spacing[200]}`};
   border-radius: ${({ theme }) => theme.radius[8]};
 
   cursor: pointer;
+
+  .text, svg {
+    vertical-align: middle;
+   }
 
   span {
     &.left {
@@ -59,14 +64,30 @@ const StyledButton = styled.button`
     &.right {
       margin-left: ${({ theme }) => theme.spacing[100]};
     }
-
-    &.text, > svg {
-      vertical-align: middle;
-    }
-
   }
 
+  svg {
+    width: 18px;
+    height: 100%;
+  }
+
+
+  /* responsive tablet */
+  ${({ theme }) => theme.media.tablet`
+    ${({ theme }) => theme.typography.textPreset5};
+    svg {
+      width: 24px;
+      height: 100%;
+    }
+  `}
+
+  /* responsive laptop */
+  ${({ theme }) => theme.media.laptop`
+    ${({ theme }) => theme.typography.textPreset4};
+  `}
+
   
+  /* theme */
   &.primary {
     color: ${({ theme }) => theme.colors.white};
     background: ${({ theme }) => theme.colors.blue500};
@@ -133,12 +154,60 @@ const StyledButton = styled.button`
 
     ${disabledStyles}
   }
-`;
 
+  &.ghost {
+    padding: ${({ theme }) => theme.spacing[25]};
+    color: ${({ theme }) => theme.colors.neutral600};
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.neutral950};
+      background: ${({ theme }) => theme.colors.neutral100};
+      svg path {
+        stroke: ${({ theme }) => theme.colors.neutral950};
+      }
+    }
+
+    &:active {
+      color: ${({ theme }) => theme.colors.neutral600};
+      svg path {
+        stroke: ${({ theme }) => theme.colors.neutral600};
+      }
+    }
+
+    &.svg_fill {
+      &:hover {
+        background: ${({ theme }) => theme.colors.neutral100};
+        svg path {
+          stroke: transparent;
+          fill: ${({ theme }) => theme.colors.neutral950};
+        }
+      }
+
+      &:active {
+        svg path {
+          stroke: transparent;
+          fill: ${({ theme }) => theme.colors.neutral600};
+        }
+      }
+    }
+
+    &.text_blue {
+      color: ${({ theme }) => theme.colors.blue500};
+      &:hover {
+        color: ${({ theme }) => theme.colors.blue700};
+      }
+
+      &:active {
+        color: ${({ theme }) => theme.colors.blue500};
+      }
+
+    }
+  }
+`;
 export default BaseButton;
 
 BaseButton.propTypes = {
-  theme: PropTypes.oneOf(['primary', 'secondary', 'border']),
+  theme: PropTypes.oneOf(['primary', 'secondary', 'border', 'ghost']),
   leftIcon: PropTypes.element,
   texture: PropTypes.string,
   rightIcon: PropTypes.element,
