a0c3f24b7ff3f1b71a9405322227288eb65087b0
hyunwleeee <hwlee3928@gmail.com>
2025-06-19 16:37:27 +0900

    feat: useFormState hook

---
+++ src/hooks/useFormState.jsx
@@ -0,0 +1,27 @@
+import { useReducer } from 'react';
+
+const useFilter = (init) => {
+  const [formState, formDispatch] = useReducer(formStateReducer, init);
+
+  return {
+    formState,
+    formDispatch,
+  };
+};
+
+const formStateReducer = (state, action) => {
+  let result = state;
+
+  Object.keys(state).forEach((key) => {
+    if (action.type === key) {
+      result = { ...state, [key]: action.value };
+    }
+  });
+
+  if (action.type === 'set-data')
+    result = action.value;
+
+  return result;
+};
+
+export default useFilter;
