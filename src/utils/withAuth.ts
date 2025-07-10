e431bc8707492ea8370dbc67baebbce1940bb049
hyunwleeee <hwlee3928@gmail.com>
2025-07-11 01:29:57 +0900

    feat: withAuth util func

---
+++ src/utils/withAuth.ts
@@ -0,0 +1,27 @@
+import { useAuthStore } from "@store/authStore";
+import { type AxiosRequestConfig, isAxiosError } from "axios";
+import { toast } from "react-toastify";
+
+type WithAuthCallback<R> = (
+  options: Partial<AxiosRequestConfig>
+) => R;
+
+
+export const withAuth = <R>(callback: WithAuthCallback<R>): R => {
+  try {
+    const { token } = useAuthStore.getState();
+
+    return callback({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error: unknown) {
+    if (isAxiosError(error) && error.response?.status === 401) {
+      toast.error('로그인이 필요합니다.');
+      window.location.replace('/login');
+    }
+    throw error;
+  }
+};
+
